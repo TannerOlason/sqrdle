52618d04f0088cb4a885132d2164c2ae1e827cc3
TannerOlason <tanner.olason@gmail.com>
2023-03-31 20:02:28 -0500

    Fixed non-game keys from being counted on gameboard.

--- src/components/layout/VirtualKeyboard.vue
+++ src/components/layout/VirtualKeyboard.vue
@@ -25,7 +25,9 @@ export default {
     const someSuccessCallback = ({ keyCode }) => {};
 
     const someAnyKeyCallback = ({ event }) => {
-      emit('keyPressed', event.key.toLowerCase())
+      if ("qwertyuiopasdfghjklzxcvbnmEnterBackspace".includes(event.key)) {
+        emit('keyPressed', event.key.toLowerCase())
+      }
     };
 
     useKeypress({
@@ -48,49 +50,57 @@ export default {
   <div class="row-1">
     <KeyButton
       v-for="i in 10"
+      tabindex="0"
       :class="'key-' + alphabetString[i - 1]"
       :letter="alphabetString[i - 1]"
       :tried="alphabet.get(alphabetString[i - 1])"
       @click="this.$emit('keyPressed', alphabetString[i - 1])"
     />
-    <div
-      class="backspace spacer top-backspace"
+    <div class="backspace spacer top-backspace"
+      tabindex="0"
       @click="this.$emit('keyPressed', 'backspace')"
     >
-    <img :src="images['arrow_left']" alt="Calcualtor icons">
+    <img :src="images['arrow_left']" alt="Backspace key icon">
     </div>
   </div>
   <div class="row-2">
     <div class="caps-lock-space spacer"></div>
     <KeyButton
       v-for="i in 9"
+      tabindex="0"
       :class="'key-' + alphabetString[i + 9]"
       :letter="alphabetString[i + 9]"
       :tried="alphabet.get(alphabetString[i + 9])"
       @click="this.$emit('keyPressed', alphabetString[i + 9])"
     />
-    <div class="enter-space spacer" @click="this.$emit('keyPressed', 'enter')">
-      <img :src="images['curve_arrow_left']" alt="Calcualtor icons">
+    <div class="enter-space spacer"
+      tabindex="0"
+      @click="this.$emit('keyPressed', 'enter')">
+      <img :src="images['curve_arrow_left']" 
+        alt="Enter key icon">
     </div>
   </div>
   <div class="row-3">
     <div class="left-shift-space spacer"></div>
     <KeyButton
       v-for="i in 7"
+      tabindex="0"
       :class="'key-' + alphabetString[i + 18]"
       :letter="alphabetString[i + 18]"
       :tried="alphabet.get(alphabetString[i + 18])"
       @click="this.$emit('keyPressed', alphabetString[i + 18])"
     />
-    <div class="dex spacer" @click="this.$emit('toggleDex')">
-      <img :src="images['calculator']" alt="Calcualtor icons">
+    <div class="dex spacer"
+      tabindex="0"
+      @click="this.$emit('toggleDex')">
+      <img :src="images['calculator']" 
+        alt="Calculator icon">
     </div>
     <div class="hints spacer">?</div>
-    <div
-      class="backspace spacer bottom-backspace"
-      @click="this.$emit('keyPressed', 'backspace')"
-    >
-    <img :src="images['arrow_left']" alt="Calcualtor icons">
+    <div class="backspace spacer bottom-backspace"
+      tabindex="0"
+      @click="this.$emit('keyPressed', 'backspace')">
+      <img :src="images['arrow_left']" alt="Backspace icon">
     </div>
   </div>
 </template>
